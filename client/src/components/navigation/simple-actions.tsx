0264c0d15854b161e26e39ae82fe17c49cc7c8ea
zhanik91 <zh.shayakhmetov@gmail.com>
2025-12-04 01:58:14 +0000

    Restored to '69b3230ac7d5bd925256243346fdd2942c24a840'
    Replit-Restored-To: 69b3230ac7d5bd925256243346fdd2942c24a840

--- client/src/components/navigation/simple-actions.tsx
+++ client/src/components/navigation/simple-actions.tsx
@@ -1,19 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Plus, FileText, Building, FileCheck } from "lucide-react";
+import { Plus, FileText } from "lucide-react";
 import { Link } from "wouter";
 
 export function SimpleActions() {
-  return (
-    <Card className="p-4">
-      <div className="flex flex-col gap-2">
-        <Link href="/controlled-objects">
-            <Button variant="outline" className="w-full justify-start">
-              <Building className="mr-2 h-4 w-4" />
-              Перечень подконтрольных объектов
-            </Button>
-          </Link>
-        </div>
-    </Card>
-  );
+  return null;
 }
